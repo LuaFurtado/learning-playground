098b845b5ff5ac182508162a8ff3bf91f6d6a9b6
Luana Furtado <luana.furtado@calbrightcollege.org>
2025-11-06 18:52:23 -0800

    pair programming

--- pair-programming.js
+++ pair-programming.js
@@ -1,26 +1,84 @@
-/*
-For Loop: Write a for loop that prints numbers from 1 to 5.
-*/
+// /*
+// For Loop: Write a for loop that prints numbers from 1 to 5.
+// */
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
 
-/*
-While Loop: Write a while loop that prints numbers from 1 to 5.
-*/
+// /*
+// While Loop: Write a while loop that prints numbers from 1 to 5.
+// */
 
-/*
-Do-While Loop: Write a do...while loop that prints numbers from 1 to 5.
-*/
+// let j = 1;
+// while (j <= 5) {
+//   console.log(j);
+//   j++;
+// }
+// /*
+// Do-While Loop: Write a do...while loop that prints numbers from 1 to 5.
+// */
 
-/*
-Switch Statement: Write a switch statement that performs different actions based on the input value:
+// let k = 1;
+// do {
+//   console.log(k);
+//   k++;
+// } while (k <= 5);
+// /*
+// Switch Statement: Write a switch statement that performs different actions based on the input value:
 
-- If the input is 'apple', output 'It's an apple.'
-- If the input is 'banana', output 'It's a banana.'
-- If the input is 'cherry', output 'It's a cherry.'
-- Otherwise, output 'Unknown fruit.'
+// let fruit = "banana"; */
 
-*/
+// switch (fruit) {
+//   case "apple":
+//     console.log("It's an apple.");
+//     break;
+//   case "banana":
+//     console.log("It's a banana.");
+//     break;
+//   case "cherry":
+//     console.log("It's a cherry.");
+//     break;
+//   default:
+//     console.log("Unknown fruit.");
+// }
 
-/*
-forEach Loop: Write a forEach loop that iterates over an array of fruits and logs each one to the console.
+// //or
 
-*/
+// function describeFruit(fruit) {
+//     let answer;
+//     switch (fruit) {
+//         case 'apple':
+//             answer = "It/'s an apple";
+//             break;
+//         case 'banana':
+//             answer = "It's a banana";
+//             break;
+//         case 'cherry':
+//             answer = "It's a crerry";
+//             break;
+//         default: "Unknown Fruit"
+//     }
+//     return answer;
+// }
+// console.log(describefruit("banana"));
+
+
+// /*
+
+// - If the input is 'apple', output 'It's an apple.'
+// - If the input is 'banana', output 'It's a banana.'
+// - If the input is 'cherry', output 'It's a cherry.'
+// - Otherwise, output 'Unknown fruit.'
+
+// */
+
+// /*
+// forEach Loop: Write a forEach loop that iterates over an array of fruits and logs each one to the console.
+
+// */
+
+// const fruits = ["apple", "banana", "cherry"];
+
+// fruits.forEach((item) => {
+//   console.log(item);
+// });
